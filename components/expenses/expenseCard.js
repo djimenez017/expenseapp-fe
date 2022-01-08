27a779d253b8d3937cf81d4c89fdff1db43730e3
Daniel Jimenez <djimenez017@gmail.com>
2022-01-08 01:12:19 -0800

    working on changes

--- components/expenses/expenseCard.js
+++ components/expenses/expenseCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 
+const expense = {
+  id: 1,
+  author: "Daniel Jimenez",
+  addedDate: "1/7/2022",
+  name: "Planet Fitness",
+  category: "fitness",
+  amount: 20.99,
+  frequency: "monthly",
+  dueDate: "1/20/2021",
+};
+
 export default function expenseCard() {
   return (
-    <div className="p-5 bg-[white] rounded-lg ">
-      <h3>Gym</h3>
-      <p>Date Added: 1/22/2022</p>
-      <p>$20.99/month</p>
-      <p>Due Date: 2/22/2022</p>
+    <div className="p-5 bg-[white] rounded-lg flex flex-row space-x-4">
+      <div className="bg-[gray] rounded-full">PF</div>
+      <div className="bg-[gray]">
+        <h3>Gym</h3>
+        <p>Date Added: 1/22/2022</p>
+        <p>$20.99/month</p>
+        <p>Due Date: 2/22/2022</p>
+      </div>
     </div>
   );
 }
