363ad6e1a8bbd9994d9903dc0955dfc8522b29df
Daniel Jimenez <danieljimenez@Daniels-MacBook-Pro.local>
2022-01-08 16:52:41 -0800

    more change to the front end

--- components/expenses/expenseCard.js
+++ components/expenses/expenseCard.js
@@ -13,9 +13,11 @@ const expense = {
 
 export default function expenseCard() {
   return (
-    <div className="p-5 bg-[white] rounded-lg flex flex-row space-x-4">
-      <div className="bg-[gray] rounded-full">PF</div>
-      <div className="bg-[gray]">
+    <div className="p-5 bg-[white] rounded-lg flex flex-row space-x-4 w-full box-border	">
+      <div className="flex flex-column justify-center basis-1/12 bg-[gray] rounded-full w-1/12">
+        <h3 className=" p-8 text-3xl font-bold">PF</h3>
+      </div>
+      <div className="bg-[gray] w-full ">
         <h3>Gym</h3>
         <p>Date Added: 1/22/2022</p>
         <p>$20.99/month</p>
