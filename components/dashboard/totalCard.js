3ac77e513e12608986fa3fc3f081fbeb74cc8342
Daniel Jimenez <danieljimenez@Daniels-MacBook-Pro.local>
2022-01-12 23:56:00 -0800

    refactored tailwind classes

--- components/dashboard/totalCard.js
+++ components/dashboard/totalCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function totalCard(props) {
   return (
-    <div className="flex flex-column justify-center basis-1/2 p-5 bg-green1 ">
+    <div className="flex flex-column justify-center basis-1/2 p-5 bg-green1  ">
       <h3 className="text-white text-2xl font-bold">Total Owed: </h3>
       <p className="text-white text-2xl font-bold">$3.749.56</p>
     </div>
