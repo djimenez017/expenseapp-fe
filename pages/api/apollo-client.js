57907a2edc29f8e6fa56702936b8d36f734e06ad
Daniel Jimenez <djimenez017@gmail.com>
2022-06-14 19:21:13 -0700

    idk

--- pages/api/apollo-client.js
+++ pages/api/apollo-client.js
@@ -3,8 +3,8 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000",
-  //uri: process.env.PPROD_BE,
+  //uri: "http://localhost:4000",
+  uri: process.env.PPROD_BE,
 });
 
 const authLink = setContext((_, { headers }) => {
